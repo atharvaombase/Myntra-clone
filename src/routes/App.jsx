d37b646020ae8f3beeeff03cf2d7a7309291bc86
Atharva Ombase <94031822+atharvaombase@users.noreply.github.com>
2025-02-18 19:02:34 +0530

    removed bugs

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -3,13 +3,11 @@ import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import LoadingSpinner from "../components/LoadingSpinner";
 function App() {
-  const fetchStatus = useSelector((store) => store.fetchStatus);
   return (
     <>
       <Header />
-      {!fetchStatus.currentlyFetching ? <Outlet /> : <LoadingSpinner />}
+      <Outlet />
       <Footer />
     </>
   );
