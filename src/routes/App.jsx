e55bcc5fb1f92a5bb1673a10c64fcbaaf93ff678
Atharva Ombase <94031822+atharvaombase@users.noreply.github.com>
2025-02-18 19:01:30 +0530

    removed bugs

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
-import Fetchitem from "../components/FetchItem";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LoadingSpinner from "../components/LoadingSpinner";
